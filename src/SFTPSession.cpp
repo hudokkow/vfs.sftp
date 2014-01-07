1d5c99bcc425856a046905ea5c371f3035081577
Arne Morten Kvarving <arne.morten.kvarving@sintef.no>
2014-01-07 17:20:20 +0100

    s

--- src/SFTPSession.cpp
+++ src/SFTPSession.cpp
@@ -198,8 +198,8 @@ bool CSFTPSession::GetDirectory(const std::string& base, const std::string& fold
 
           if (attributes->flags & SSH_FILEXFER_ATTR_ACMODTIME)
           {
-            entry.mtime.dwLowDateTime = attributes->mtime & ((1LL << 32)-1);
-            entry.mtime.dwHighDateTime = attributes->mtime >> 32;
+            entry.mtime.dwLowDateTime = attributes->mtime64 & ((1LL << 32)-1);
+            entry.mtime.dwHighDateTime = attributes->mtime64 >> 32;
           }
 
           if (attributes->type & SSH_FILEXFER_TYPE_DIRECTORY)
